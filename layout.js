f59e9b5f09cecc0a9f853e03ec0606f373b5e2ef
leizhou501 <lzhouworking501@gmail.com>
2025-09-08 05:23:35 +0800

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,15 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Baccarat Predictor',
+  description: 'AI-powered Baccarat Analysis Tool',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-TW">
+      <body>{children}</body>
+    </html>
+  )
+}
+
